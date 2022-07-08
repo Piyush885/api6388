417ea343654dd308d3f7d781b366fc1e901993d1
unknown <raipiyush@gmail.comgit config --global user.name Piyushgit config --global user.email raipiyush@gmail.com>
2022-07-08 06:05:36 -0700

    f

--- script.py
+++ script.py
@@ -9,12 +9,11 @@ def index():
     # response=requests.get('https://brl1test.herokuapp.com/').json()
     # print(response)
     # return HttpResponse("hello")
-    schedule.every(5).seconds.do(index2)    
+    schedule.every(30).minutes.do(index2)    
     schedule.run_pending()
     while True:
         schedule.run_pending()
         time.sleep(1)
 def index2():
     response=requests.get('https://brl1test.herokuapp.com/').json()
-    print(response)
 print(index())
