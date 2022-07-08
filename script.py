1850266f589f5a77952382899659b5f80f129445
unknown <raipiyush@gmail.comgit config --global user.name Piyushgit config --global user.email raipiyush@gmail.com>
2022-07-07 23:31:08 -0700

    first commit

---
+++ script.py
@@ -0,0 +1,20 @@
+import time
+
+# while True:
+# 	print("I am working!")
+# 	time.sleep(2)
+import requests
+import schedule
+def index():
+    # response=requests.get('https://brl1test.herokuapp.com/').json()
+    # print(response)
+    # return HttpResponse("hello")
+    schedule.every(5).seconds.do(index2)    
+    schedule.run_pending()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+def index2():
+    response=requests.get('https://brl1test.herokuapp.com/').json()
+    print(response)
+print(index())
